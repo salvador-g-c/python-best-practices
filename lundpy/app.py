6e61929b62b34d27895ae32bebc5e334217f8a40
salvador-g-c <sagm@lundbeck.com>
2024-10-21 11:33:59 +0200

    This is a the answer to somethig to do.

--- lundpy/app.py
+++ lundpy/app.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 quotes = [
     {"text": "Code is like humor. When you have to explain it, it’s bad.", "author": "Cory House"},
     {"text": "Fix the cause, not the symptom. A thing to do.", "author": "Steve Maguire"},
-    {"text": "Optimism is an occupational hazard of programming.", "author": "Kent Beck"},
+    {"text": "Optimism is an occupational hazard of programming. A test.", "author": "Kent Beck"},
     {"text": "Simplicity is the soul of efficiency.", "author": "Austin Freeman"}
 ]
 
